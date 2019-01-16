25e8949acccc045adb06d23e4d73464697d82274
Ru Lette <Buckinrich@Rus-MacBook-Air.broadband>
2019-01-16 01:11:56 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,29 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+
+
+function flipCard(cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log ("User flipped " + cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+
+ function checkForMatch() {
+  //if (cardsInPlay.length == 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          console.log("You found a match!");
+          //setTimeout('location', 50000);
+        //location.reload()
+
+      } else {
+          console.log("Sorry, try again.");
+        };
+}
+
+
+
